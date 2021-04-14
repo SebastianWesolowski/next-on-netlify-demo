007b4c0adceb6401402d4a058ae120476cbf70ed
SebastianWesolowskiBpol <sebastian.wesolowski@bpol.net>
2021-04-15 00:20:45 +0200

    change

--- pages/getServerSideProps/show.js
+++ pages/getServerSideProps/show.js
@@ -1,54 +1,60 @@
 import Link from "next/link";
 
-const Show = ({ show }) => (
-  <>
-    <h1>getServerSideProps: basic page</h1>
-    <p>This page uses getServerSideProps() to fetch a TV show from an API.</p>
-    <p>
-      getServerSideProps() never runs on the client.
-      <br />
-      When navigating client-side, getServerSideProps() runs inside a Netlify
-      Function. The resulting JSON data is used to render this page.
-      <br />
-      When requesting this page directly, the entire page is rendered
-      server-side by a Netlify Function.
-    </p>
-    <a
-      href="https://github.com/FinnWoelm/next-on-netlify-demo/tree/master/pages/getServerSideProps/show.js"
-      target="_blank"
-    >
-      View code on GitHub
-    </a>
+const Show = ({ show }) => {
 
-    <hr />
+  console.log(show)
+  return (
+      <>
+        <h1>getServerSideProps: basic page</h1>
+        <p>This page uses getServerSideProps() to fetch a TV show from an API.</p>
+        <p>
+          getServerSideProps() never runs on the client.
+          <br/>
+          When navigating client-side, getServerSideProps() runs inside a Netlify
+          Function. The resulting JSON data is used to render this page.
+          <br/>
+          When requesting this page directly, the entire page is rendered
+          server-side by a Netlify Function.
+        </p>
+        <a
+          href="https://github.com/FinnWoelm/next-on-netlify-demo/tree/master/pages/getServerSideProps/show.js"
+          target="_blank"
+        >
+          View code on GitHub
+        </a>
 
-    <h2>
-      Show #{show.id}: {show.name}
-    </h2>
-    <a href={`https://api.tvmaze.com/shows/${show.id}`} target="_blank">
-      https://api.tvmaze.com/shows/{show.id}
-    </a>
+        <hr/>
+        {show.map((node) => (
+          <h2>
+            Show #{node.id}: {node.slug}
+          </h2>
+        ))}
 
-    <p>
-      Type: {show.type} <br />
-      Language: {show.language} <br />
-      Status: {show.status} <br />
-      Premiered: {show.premiered} <br />
-      Official Site: {show.officialSite} <br />
-      Rating (average): {show.rating?.average}
-    </p>
+        {/*<a href={`https://api.tvmaze.com/shows/${show.id}`} target="_blank">*/}
+        {/*  https://api.tvmaze.com/shows/{show.id}*/}
+        {/*</a>*/}
 
-    <hr />
+        {/*<p>*/}
+        {/*  Type: {show.type} <br />*/}
+        {/*  Language: {show.language} <br />*/}
+        {/*  Status: {show.status} <br />*/}
+        {/*  Premiered: {show.premiered} <br />*/}
+        {/*  Official Site: {show.officialSite} <br />*/}
+        {/*  Rating (average): {show.rating?.average}*/}
+        {/*</p>*/}
 
-    <Link href="/">
-      <a>Go back home</a>
-    </Link>
-  </>
-);
+        <hr/>
+
+        <Link href="/">
+          <a>Go back home</a>
+        </Link>
+      </>
+  )
+};
 
 export async function getServerSideProps() {
   // Get the show
-  const res = await fetch("https://api.tvmaze.com/shows/263");
+  const res = await fetch("http://blog.darmowewebinary.pl/wp-json/wp/v2/posts");
   const show = await res.json();
 
   return { props: { show } };
